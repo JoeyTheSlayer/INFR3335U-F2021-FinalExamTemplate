03bb2e1a5a2b4264f8aa98949ee215b50510bfe3
JoeyTheSlayer <joseph.carrillo@ontariotechu.net>
2021-12-15 19:16:49 -0500

    Multiplayer Test
    hopefully this works

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.01f;
+    float turnSmooth;
+
+    public Joystick joystick;
+
+    public PhotonView view;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (view.IsMine)
+        {
+            float horizontal = joystick.Horizontal;
+            float vertical = joystick.Vertical;
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if (direction.magnitude >= 0.01f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmooth, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+                controller.Move(direction * speed * Time.deltaTime);
+            }
+        }
+    }
+}
